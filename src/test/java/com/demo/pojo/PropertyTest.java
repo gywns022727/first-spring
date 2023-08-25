c0fe9d59505799f1780a062d3c169f7fa91640bc
gywns022727 <lifford050227a@gmail.com>
2023-08-26 00:47:40 +0900

    project setting finish

--- src/test/java/com/demo/pojo/PropertyTest.java
+++ src/test/java/com/demo/pojo/PropertyTest.java
@@ -1,9 +1,13 @@
 package com.demo.pojo;
 
+import jakarta.servlet.annotation.WebServlet;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.List;
